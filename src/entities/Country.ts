d9b09e6d87190ff69a7b46129b44a6aed8956dc2
ALICIA <alicia.magro@gmail.com>
2025-09-15 09:49:16 +0200

    feat: create Country entity

---
+++ src/entities/Country.ts
@@ -0,0 +1,26 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Field, ObjectType } from "type-graphql";
+
+@Entity()
+@ObjectType()
+export class Country extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  @Field()
+  id: number;
+
+  @Column()
+  @Field()
+  name: string;
+
+  @Column()
+  @Field()
+  code: string;
+
+  @Column()
+  @Field()
+  emoji: string;
+
+  @Column()
+  @Field()
+  continent: string;
+}
